d98d0219fcb20bdc7f4488e7ff4a6e2e4ee28f30
Nicholas Byfleet <nbyfleet@me.com>
2017-02-18 01:05:39 +0000

    Adding validation to loans

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,5 @@
 class Loan < ApplicationRecord
   belongs_to :project
-  validates :name, presence: true
+  validates :name, :amount, :rate, presence: true
+  validates :amount, :rate, numericality: { greater_than: 0 }
 end
